01a5bce7d49a7d66c8e7a9f5b9abfaf1d5fef545
Samuel Steele <steelesam72@gmail.com>
2016-03-21 23:28:54 -0400

    Initial commit

---
+++ mouth.js
@@ -0,0 +1,61 @@
+var express = require('express'),
+    handlebars = require('express-handlebars'),
+    bodyParser = require('body-parser'),
+    Twitter = require('twitter')
+
+var app = express()
+
+// var dotenv = require('dotenv').config()
+
+var twit = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'))
+
+app.engine('handlebars', handlebars({
+    defaultLayout: 'default'
+}))
+app.set('view engine', 'handlebars')
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+app.post('/', function(req, res) {
+    if (req.body.status) {
+        twit.post('/statuses/update', {
+            status: req.body.status
+        }, function(err, tweet, response) {
+            if (err) {
+                res.render('index', {
+                    success: false,
+                    error: err[0]
+                })
+                console.log(err)
+            } else {
+                res.render('index', {
+                    success: true
+                })
+            }
+        })
+    } else {
+        res.render('index', {
+            success: false,
+            error: "Internal Server Error"
+        })
+    }
+})
+
+
+var server = app.listen(process.env.PORT || 5000, function() {
+    console.log("Server running at http://0.0.0.0:%d", server.address().port)
+})
